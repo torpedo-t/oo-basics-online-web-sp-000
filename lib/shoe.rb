e93b9cb56d2e0d35aa3eb2a4cbdb53c19b4de4c7
Thomas John Helin <thomashelin@yahoo.com>
2020-04-27 18:25:55 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,8 @@
 # Make your shoe class here!
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :
+  attr_reader :brand
+  
   def initialize(brand)
     @brand = brand
   end
